334a74fe57ecf8850f343046cac7157ce4c5daba
Andre13500 <andresbj1660@gmail.com>
2026-06-13 09:46:28 -0500

    first version

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Electronic_sys/',
   plugins: [react()],
   server: {
     port: 5173,
